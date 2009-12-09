a7be7b504b1657d884394e91972aa010eae1f3d6
Brendan Kemp <brendan.kemp@gmail.com>
2009-12-09 18:14:36 -0500

    Implemented the incoming_tweets association.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 
   has_many :tweets, :foreign_key => 'author_id'
-  
-  has_many :outgoing_subscriptions, 
+
+  has_many :outgoing_subscriptions,
     :class_name => 'Subscription',
     :foreign_key => 'follower_id'
 
@@ -10,22 +10,24 @@ class User < ActiveRecord::Base
     :class_name => 'User',
     :through => :outgoing_subscriptions
 
-  has_many :incoming_subscriptions, 
+  has_many :incoming_subscriptions,
     :class_name => 'Subscription',
     :foreign_key => 'following_id'
-    
+
   has_many :followers,
     :class_name => 'User',
     :through => :outgoing_subscriptions
-    
+
   has_many :incoming_tweets, :class_name => 'Tweet',
-    :finder_sql => 'SELECT * FROM tweets WHERE tweets.author_id IN (#{following_ids.join(",")})',
+    :finder_sql => 'SELECT * FROM tweets WHERE tweets.author_id IN ("#{following_ids.join(",")}")',
     :order => 'created_at DESC'
-  
+
   # attributes: username, name, bio
-  
+
   validates_presence_of :username, :name
   validates_length_of :bio, :within => Tweet::RANGE
-  
 
+  def following_ids
+    self.following.collect { |following| following.id }
+  end
 end
